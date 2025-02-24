4cc7007d6c369819375eaf6e70c17b8604c24cf2
Ali <alicantokdemir@gmail.com>
2025-02-24 12:14:34 +0100

    feat(button): ui button for uniformity

---
+++ src/components/ui/button.js
@@ -0,0 +1,61 @@
+import {LitElement, html, css} from 'lit';
+
+class AppButton extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+    }
+    .app-button {
+      padding: var(--spacing-2) var(--spacing-4);
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      width: 100%;
+
+      &.primary {
+        background-color: var(--primary-color);
+        color: white;
+      }
+
+      &.secondary {
+        background-color: var(--secondary-color);
+        color: white;
+      }
+
+      &.light {
+        background-color: transparent;
+        border: 1px solid var(--secondary-color);
+        color: black;
+      }
+    }
+  `;
+
+  static properties = {
+    type: {type: String},
+    className: {type: String},
+  };
+
+  constructor() {
+    super();
+    this.type = 'button';
+    this.className = '';
+  }
+
+  handleClick(event) {
+    this.dispatchEvent(new CustomEvent('button-click', {detail: event}));
+  }
+
+  render() {
+    return html`
+      <button
+        type="${this.type}"
+        @click="${this.handleClick}"
+        class="app-button ${this.className}"
+      >
+        <slot></slot>
+      </button>
+    `;
+  }
+}
+
+customElements.define('app-button', AppButton);
